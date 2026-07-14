d67eff23b86bc040a777a4cc07b3d965eab3a897
Pohur Navin <navin.pohur@cygest.fr>
2026-07-14 22:04:47 +0400

    week 3

---
+++ week3s1.py
@@ -0,0 +1,68 @@
+
+# def greetUser(name):
+#     print("Hello" + " " + name)
+
+# def greetUser(studentName = "Allan"):
+#     print("Hello" + " " + studentName) 
+#     #print("Hello", studentName)
+
+#     # Hello Allan
+
+# greetUser("Fatine")
+
+
+# def greetUser(name: str):
+
+#     if not isinstance(name, str):
+#         print("Not a string")
+#     else:
+#         print("Hello", name)
+
+# greetUser("ddd")
+
+# x = 18
+# if(x >= 18):
+#     print("Adult")    
+# else:
+#     print("minor")
+# print("what is your age again?")
+
+
+# HTML TAGS
+
+# <body> </body>
+# <p> </p>
+
+
+# def GenerateHtml(tag, value):
+#     if(len(tag) > 0 and len(value) > 0):  # in other languages and is written as && / len() => calculates the length of the string
+#         print("<" + tag + ">" + value + "</" + tag + ">")
+#     else:
+#         print("wrong input!!")
+
+
+
+# GenerateHtml("h1", "Akiola")
+# GenerateHtml("p", "this is a title")
+
+
+# FUNCION RETURN
+
+# def CalculateArea(width, lngth):
+#     if width > 0 and lngth > 0:
+#         area = width * lngth
+#         return area
+
+
+# Area = CalculateArea(5, 8)
+# # print(Area + " " +"m2")
+# print(Area, "m2")
+
+
+def CalculateDivision(num1, num2):
+    if(num2 != 0):
+        return num1/num2
+
+answer = CalculateDivision(5,2)
+
+print(answer)
